foxminchan/BookWorm

chore: Update database context and repository names in BookWorm.Ordering

src/BookWorm.Ordering/Infrastructure/Mediator/MediatorExtension.cs
@@ -1,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using BookWorm.Core.SeedWork;
+using MediatR;
+
+namespace BookWorm.Ordering.Infrastructure.Data;
+
+public static class MediatorExtension
+{
+    public static async Task DispatchDomainEventsAsync(this IPublisher mediator, OrderingContext ctx)
+    {
+        var domainEntities = ctx.ChangeTracker
+            .Entries<EntityBase>()
+            .Where(x => x.Entity.DomainEvents.Count != 0)
+            .ToImmutableList();
+
+        var domainEvents = domainEntities
+            .SelectMany(x => x.Entity.DomainEvents)
+            .ToImmutableList();
+
+        domainEntities.ForEach(entity => entity.Entity.ClearDomainEvents());
+
+        foreach (var domainEvent in domainEvents)
+            await mediator.Publish(domainEvent);
+    }
+}

aleevan2812/BookWorm_cloned

src/BookWorm.Ordering/Infrastructure/Mediator/MediatorExtension.cs
@@ -1,0 +1,28 @@
+﻿using System.Collections.Immutable;
+using BookWorm.Core.SeedWork;
+using BookWorm.Ordering.Infrastructure.Data;
+using MediatR;
+
+namespace BookWorm.Ordering.Infrastructure.Mediator;
+
+public static class MediatorExtension
+{
+    public static async Task DispatchDomainEventsAsync(this IPublisher mediator, OrderingContext ctx)
+    {
+        var domainEntities = ctx.ChangeTracker
+            .Entries<EntityBase>()
+            .Where(x => x.Entity.DomainEvents.Count != 0)
+            .ToImmutableList();
+
+        var domainEvents = domainEntities
+            .SelectMany(x => x.Entity.DomainEvents)
+            .ToImmutableList();
+
+        domainEntities.ForEach(entity => entity.Entity.ClearDomainEvents());
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await mediator.Publish(domainEvent);
+        }
+    }
+}